889eb3bda1ba6c4bea0ec9542ac985e388e42c32
pushpendra <pushpendra@captalnumbers.com>
2021-09-23 11:14:19 +0530

    licence

--- great_text/great.py
+++ great_text/great.py
@@ -4,11 +4,14 @@ from termcolor import colored
 
 def great_text(text: str = "Hello", color: str = "red", font: str = "None"):
     if font == "None":
-        print(colored(text, color))
+        print(colored(text, color, attrs=["reverse", "blink"]))
+        import os
+
+        os.system("pause")
     else:
         f = Figlet(font=font)
         print(colored(f.renderText(text), color))
 
 
 if __name__ == "__main__":
-    great_text("Hello", "red", "isometric2")
+    great_text("Hello", "red")
